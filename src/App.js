1d5cbd15ca47756c9fd9a8fb05021ef5a7e10158
Nazar <kostyrkon@gmail.com>
2020-03-29 13:18:18 +0100

    Naming corrected according to request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import FormContainer from "./formContainer/FormContainer";
+import FormContainer from "./FormContainer/FormContainer";
 
 function App() {
   return (
