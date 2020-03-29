1ab8e267b98fa175ba891fabcac2465fe1f8d40e
Serhii <serhii@a-dam.com>
2020-03-29 15:46:25 +0200

    Reverted removing FormContainer

---
+++ src/FormContainer/FormContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Form from '../Form/Form.js';
+
+import './styles.css'
+
+ function FormContainer() {
+
+    return(
+        <div className='formContainer'>
+            <Form/>
+        </div>
+    )
+}
+
+export default FormContainer
+
