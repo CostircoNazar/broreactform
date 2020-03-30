c2e3cf58d1ed6fd21ceb0bd0a76097ea992d9918
Nazar <kostyrkon@gmail.com>
2020-03-30 11:10:11 +0100

    Added TapBar directory with TabBarButton and TabBar

--- src/FormContainer/FormContainer.js
+++ src/FormContainer/FormContainer.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Form from '../Form/Form.js';
 import './styles.css'
+import TabBar from "../TabBar/TabBar";
 
  function FormContainer() {
 
     return(
         <div className='formContainer'>
+            <TabBar/>
             <Form/>
+
         </div>
     )
 }
